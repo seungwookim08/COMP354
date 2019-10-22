49abdf33c3da4850a73a1dbfaa17424aaf68fd9e
Alex Alessi <aalessi18@hotmail.com>
2019-10-22 14:26:52 -0400

    Fixed merged conflict

--- ecommerce_website/src/App.js
+++ ecommerce_website/src/App.js
@@ -1,16 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 import NavBar from "./Components/js/NavBar";
-<<<<<<< HEAD
-import { SearchBar } from "./Components/js/SearchBar";
-import items from "./Components/data/items.json";
-import { ItemList } from "./Components/js/ItemList";
-import { PriceFilter } from "./Components/js/PriceFilter";
-import { CategoryFilter } from "./Components/js/CategoryFilter";
-=======
 import HomePage from "./Components/js/HomePage/HomePage";
 import AccountDashboard from "./Components/js/AccountDashboard/AccountDashboard";
->>>>>>> master
 
 class App extends Component {
   constructor() {
