77d66e3a380c11b9c7243b1aec462940a7487649
Alex Alessi <aalessi18@hotmail.com>
2019-10-30 10:25:27 -0400

    Removed unnecessary import

--- ecommerce_website/src/Components/js/HomePage/Item.jsx
+++ ecommerce_website/src/Components/js/HomePage/Item.jsx
@@ -4,7 +4,7 @@ import Card from '@material-ui/core/card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import {withRouter, Route} from 'react-router-dom';
+import {withRouter} from 'react-router-dom';
 import {connect} from 'react-redux';
 import {addItem} from '../../../Redux/cart/cart.actions';
 
