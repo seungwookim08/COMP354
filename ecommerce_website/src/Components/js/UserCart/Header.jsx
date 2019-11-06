466e8947f2ee2fdebd3e9c446e166d08d82f8f35
Patrick <patrick.leduc13@gmail.com>
2019-11-06 13:28:54 -0500

    redux works for signing in user, user info saved in state, sign out button removes info from state

--- ecommerce_website/src/Components/js/UserCart/Header.jsx
+++ ecommerce_website/src/Components/js/UserCart/Header.jsx
@@ -3,18 +3,35 @@ import CartIcon from "./CartIcon";
 import CartDropdown from "./CartDropdown";
 import {connect} from "react-redux";
 import { Typography } from '@material-ui/core';
+import {logoutCurrentUser} from '../../../Redux/user/user.actions';
+import {Link} from 'react-router-dom';
+import {Button} from '@material-ui/core';
 
-const Header = ({hidden, user}) => (
-    <div className="icon-outer">
-        <Typography> {user} </Typography>
-        <CartIcon/>
-        {hidden ? null : <CartDropdown /> }
+const Header = ({hidden, currentUser, logoutCurrentUser}) => (
+    <div>
+    <div className="header" width="10%">
+    <Typography> 
+            {currentUser == null ? "" : "Welcome, " + currentUser}
+          </Typography>
     </div>
+    <div className="header">
+      <Typography> 
+            {currentUser == null ? <Link to="/login"> Sign In</Link> : <Link onClick={() => logoutCurrentUser()}>Sign Out</Link>}
+          </Typography>
+      </div>
+      <div className="icon-outer">
+          <CartIcon/>
+          {hidden ? null : <CartDropdown /> }
+      </div>
+      </div>
 );
 
 const mapStateToProps = state => ({
   hidden: state.cart.hidden,
-  currentUser: state.user
+  currentUser: state.user.currentUser
 });
+const mapDispatchToProps = dispatch => ({
+  logoutCurrentUser:() => dispatch(logoutCurrentUser())
+})
 
-export default connect(mapStateToProps)(Header);
+export default connect(mapStateToProps,mapDispatchToProps)(Header);
