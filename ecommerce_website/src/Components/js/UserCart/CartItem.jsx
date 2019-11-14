69b3b40d1113e12f538ebbff98d0f0676e6e8093
Patrick <patrick.leduc13@gmail.com>
2019-11-14 15:34:46 -0500

    checkout and cart have images of product

--- ecommerce_website/src/Components/js/UserCart/CartItem.jsx
+++ ecommerce_website/src/Components/js/UserCart/CartItem.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import '../../css/Cart.css';
 
-const CartItem = ({item: {id, imageUrl, price, name, quantity} }) => (
+const CartItem = ({item: {id, images, price, name, quantity} }) => (
     <div className='cart-item'>
-       <img src={imageUrl}alt='item'/>
+       <img src={images} alt='item'/>
        <div className='item-details'>
           <span className='name'> {name} </span> 
           <span className='name'> {quantity} x ${price} </span> 
