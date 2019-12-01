398de394bbca2ce46a258ec632811fd5b47336ec
Alex Alessi <aalessi18@hotmail.com>
2019-12-01 13:58:15 -0500

    Added basic dialog box for when we click on add to cart

--- ecommerce_website/src/Components/js/HomePage/Item.jsx
+++ ecommerce_website/src/Components/js/HomePage/Item.jsx
@@ -4,12 +4,41 @@ import Card from '@material-ui/core/card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Slide from '@material-ui/core/Slide';
 import {withRouter} from 'react-router-dom';
 import {connect} from 'react-redux';
 import {addItem} from '../../../Redux/cart/cart.actions';
 
+const Transition = React.forwardRef(function Transition(props, ref) {
+    return <Slide direction="up" ref={ref} {...props} />;
+});
+
 const Item = ({item, history, addItem}) => {
     const {id, name, price, images, category, manufacturer} = item;
+    const [open, setOpen] = React.useState(false);
+
+    const handleClickOpen = (item) => {
+        setOpen(true);
+    };
+
+    function confirmPressed(item) {
+        addItem(item)
+        handleClose();
+    }
+
+    function cancelPressed() {
+        setOpen(false);
+    }
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
     return(
         <Card>
             <CardContent
@@ -27,7 +56,35 @@ const Item = ({item, history, addItem}) => {
                 </div>
             </CardContent>
             <div className="button">
-                <Button variant="outlined" onClick={() => addItem(item)}>Add To Cart</Button>
+                <Button variant="outlined" onClick={() => handleClickOpen(item)}>Add To Cart</Button>
+                <Dialog
+                    className="dialog-container"
+                    open={open}
+                    TransitionComponent={Transition}
+                    keepMounted
+                    onClose={handleClose}
+                    aria-labelledby="alert-dialog-slide-title"
+                    aria-describedby="alert-dialog-slide-description"
+                >
+                    <DialogTitle id="alert-dialog-slide-title">{"Add to cart?"}</DialogTitle>
+                        <DialogContent>
+                            <DialogContentText id="alert-dialog-slide-description">
+                                <img alt="item" src={images[0]} style={{width: 200, height: 200}} />
+                                <Typography variant="h5" component="h2"> {name} </Typography>
+                                <Typography> Price: {price} $</Typography>
+                                <Typography> Category: {category} </Typography>
+                                <Typography> Manufacturer: {manufacturer} </Typography>
+                            </DialogContentText>
+                        </DialogContent>
+                    <DialogActions>
+                        <Button onClick={() =>cancelPressed()} color="primary">
+                            Cancel
+                        </Button>
+                        <Button onClick={() => confirmPressed(item)} color="primary">
+                            Confirm
+                        </Button>
+                    </DialogActions>
+                </Dialog>
             </div>
         </Card>
 )};
