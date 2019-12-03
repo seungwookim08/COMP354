d6e29176674490f6a4cf5ed79e11d0fbd5cb1e55
Alex Alessi <aalessi18@hotmail.com>
2019-12-03 11:06:56 -0500

    Revert "Merge pull request #95 from seungwookim08/emailNotifications"
    This reverts commit b1578b90e2b74fa212cc8954288e4094a5142fcb, reversing
    changes made to fba4874a96d0a14d38cc2730060984a70edc7958.

--- ecommerce_website/src/Components/js/UserCart/CheckoutPage.jsx
+++ ecommerce_website/src/Components/js/UserCart/CheckoutPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import axios from "axios";
 import '../../css/Cart.css';
 import Table from '@material-ui/core/Table';
@@ -6,175 +6,123 @@ import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
-import { connect } from "react-redux";
-import { createStructuredSelector } from 'reselect';
+import {connect} from "react-redux";
+import {createStructuredSelector} from 'reselect';
 import { selectCartItems, selectCartTotal } from '../../../Redux/cart/cart.selectors';
 import { selectUser } from '../../../Redux/user/user.selectors';
 import CheckoutItem from "./CheckoutItem";
 import { PayPalButton } from "react-paypal-button-v2";
 
+import { Redirect } from 'react-router-dom';
 import L from '@material-ui/core/Link';
 import { Link } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-
-function CheckoutPage({ cartItems, total, user }) {
-
-    const [checkOne, setCheckOne] = useState(true);
-    const [checkTwo, setCheckTwo] = useState(false);
-    const [address, setAddress] = useState(user.primaryAddress);
-
-    const handleChange = name => event => {
-
-        if (name == "checkOne") {
-            setCheckOne(true);
-            setCheckTwo(false);
-            console.log(checkOne);
-            console.log(checkTwo);
-            setAddress(user.primaryAddress);
-        }
-        if (name == "checkTwo") {
-            setCheckOne(false);
-            setCheckTwo(true);
-            console.log(checkOne);
-            console.log(checkTwo);
-            setAddress(user.alternateAddress);
-        }
-    }
 
 
-    return (
-        <div>
-            {cartItems.length ?
-                (
-                    <div className="checkout">
-                        <Table size="small">
-                            <TableHead>
-                                <TableRow>
-                                    <TableCell>Product</TableCell>
-                                    <TableCell>Description</TableCell>
-                                    <TableCell> Quantity </TableCell>
-                                    <TableCell> Price </TableCell>
-                                    <TableCell align="right">Remove</TableCell>
-                                </TableRow>
-                            </TableHead>
-                            <TableBody>
-                                {cartItems.map(cartItem => (
-                                    <CheckoutItem key={cartItem.id} cartItem={cartItem} />
-                                ))}
-                            </TableBody>
-                        </Table>
+// import {toggleCartHidden} from "../../../Redux/cart/cart.actions";
 
-                        <p></p><br></br>
 
-                        <div class="center_total">
+const CheckoutPage = ({cartItems, total, user}) => (
+    <div>
+    {cartItems.length ? 
+    (
+    <div className="checkout">
+        <Table size="small">
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Product</TableCell>
+                        <TableCell>Description</TableCell>
+                        <TableCell> Quantity </TableCell>
+                        <TableCell> Price </TableCell>
+                        <TableCell align="right">Remove</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    { cartItems.map(cartItem => (
+                        <CheckoutItem key={cartItem.id} cartItem={cartItem}/>
+                    ))}
+                </TableBody>
+            </Table>
 
-                            SubTotal: ${total}
-                            <p></p>
-                            Taxes (15% QC + 8% commission):  ${(total * 0.15 + total * 0.08).toFixed(2)}
-                            <p></p>
-                            Total: ${(total * 1.23).toFixed(2)}
-                            <p></p>
+    <p></p><br></br>
 
-                        </div><p></p><br></br>
+        <div class="center_total">
 
-                        {user.alternateAddress != 0 && user.sellerId != null ? (
-                            <div class="">
-                                Please choose a shipping address.
-                        <p></p>
+        SubTotal: ${total} 
+        <p></p>
+        Taxes (15% QC + 8% commission):  ${(total*0.15 + total*0.08).toFixed(2)}
+        <p></p>
+        Total: ${(total*1.23).toFixed(2)}
+        <p></p>
 
-                                <Table size="small">
-                                    <TableHead >
-                                        <TableRow>
-                                            <TableCell>Primary Address </TableCell>
-                                            <TableCell>Alternate Address </TableCell>
-                                        </TableRow>
-                                    </TableHead>
-                                    <TableBody>
-                                        <TableCell>
-                                        <p> {user.primaryAddress}</p>
-                                        <FormControlLabel
-                                            control={
-                                                <Checkbox
-                                                    onChange={handleChange("checkOne")}
-                                                    checked={checkOne}
-                                                    color="black"
-                                                />
-                                            } label="Primary"
-                                        />
-                                    </TableCell>
-                                    <TableCell>
-                                        <p> {user.alternateAddress}</p>
-                                        <FormControlLabel
-                                            control={
-                                                <Checkbox
-                                                    onChange={handleChange("checkTwo")}
-                                                    checked={checkTwo}
-                                                    color="black"
-                                                />
-                                            } label="Alternate"
-                                        />
-                                        </TableCell>
-                                    </TableBody>
-                                </Table>
-                            </div>
-                        ) : (<p></p>)
-                        }
-                        <p></p> <br></br>
+        </div><p></p><br></br>
+         
+        {user.sellerId != null ?  (
+            <div class="pay_button">  
 
-                        {user.sellerId != null ? (
-                            <div class="pay_button">
-
-                                <PayPalButton
-                                    amount={total}
-                                    onCancel={(details, data) => {
-                                        //console.log(address)
-                                        axios.post('https://rocky-shore-99218.herokuapp.com/users/' + user.sellerId + '/orders', { "shippingAddress": address }, {})
-                                            .then(function (response) {
-                                                console.log(response.data.is_success);
-                                                console.log(response.data.message);
-                                                if (response.data.is_success == true)
-                                                    alert("Thank you for purchasing from 354thestars");
-                                            })
-                                            .catch(function (error) {
-                                                console.log(error);
-                                                alert("Something went wrong. Please try again");
-                                            });
-                                    }}
-                                />
-                            </div>
-                        )
-                            :
-                            (
-                                <div class="pay_button">
-                                    <L variant="body2" label="Login" component={Link} to={"/Login"} >
-                                        <Button
-                                            fullWidth="true"
-                                            color="default"
-                                            variant="outlined"
-                                        > Please Log In Before Purchase
-                                    </Button>
-                                    </L>
-                                </div>
-                            )
-                        }
-                    </div>
+                   <PayPalButton
+                        amount={total}
+                        onCancel={(details, data)  => {
+                                axios.post('https://rocky-shore-99218.herokuapp.com/users/'+user.sellerId+'/orders', {"shippingAddress":user.primaryAddress}, {})
+                              .then(function (response) {
+                                console.log(response.data.is_success);
+                                console.log(response.data.message);
+                                if(response.data.is_success == true)
+                                    alert("Thank you for purchasing from 354thestars");
+                              })
+                              .catch(function (error) {
+                                console.log(error);
+                              });
+                        }}
+                    />
+            </div>
+            )
+            : 
+            (
+            <div class="pay_button"> 
+             <L variant="body2" label="Login" component={Link} to={"/Login"} > 
+                <Button
+                    fullWidth="true"
+                    color="primary"
+                    variant="outlined"
+                    > Please Log In Before Purchase 
+                </Button>
+            </L>
+            </div>
                 )
-                :
-                (
-                    <div align="center"> <h2> Your cart is empty </h2></div>
-                )
-            }
-        </div>
+        }
+    </div>
+    ) 
+    :
+    (
+        <div align="center"> <h2> Your cart is empty </h2></div>
     )
-}
+    }
+    </div>
+)
+
+
 
 const mapStateToProps = createStructuredSelector({
     user: selectUser,
-    cartItems: selectCartItems,
-    total: selectCartTotal,
+    cartItems:selectCartItems,
+    total:selectCartTotal
 })
-
 export default connect(mapStateToProps)(CheckoutPage);
 
+/*
+onSuccess={(details, data) => {
+alert("Transaction completed by " + details.payer.name.given_name);
+return fetch("/paypal-transaction-complete", {
+method: "post",
+body: JSON.stringify({
+orderID: data.orderID
+})
+});
+}}
+*/
+/*options={{
+clientId: "AWzSg6-sl6mTpYBnxEvA8MTgVuH_lL0iBkGggYnx3nsQV55WP5JqMiKVEQzunnskanuBz2WKl5fZzNxC"
+,currency:"CAD"
+}}
+*/
